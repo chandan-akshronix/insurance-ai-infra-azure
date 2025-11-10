712a215efbf56a9816f1a46c71ba8f0f879cf936
mayur0522 <mayurbhosalen9@gmail.com>
2025-11-10 14:24:49 +0530

    updated network file

--- network.tf
+++ network.tf
@@ -1,32 +1,4 @@
 ##############################
-# Resource Group
-##############################
-resource "azurerm_resource_group" "insurance" {
-  name     = "insurance-${terraform.workspace}-rg"
-  location = var.location
-}
-
-##############################
-# Virtual Network
-##############################
-resource "azurerm_virtual_network" "vnet" {
-  name                = "insurance-${terraform.workspace}-vnet"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.insurance.name
-  address_space       = ["10.0.0.0/16"]
-}
-
-##############################
-# Subnet
-##############################
-resource "azurerm_subnet" "subnet" {
-  name                 = "insurance-${terraform.workspace}-subnet"
-  resource_group_name  = azurerm_resource_group.insurance.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-##############################
 # Network Security Group
 ##############################
 resource "azurerm_network_security_group" "nsg" {
@@ -81,5 +53,18 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-}
 
+  # ✅ New Rule for MCP Server
+  security_rule {
+    name                       = "MCP-Server"
+    priority                   = 140
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8088"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    description                = "Allow inbound access to MCP Server on port 8088"
+  }
+}
